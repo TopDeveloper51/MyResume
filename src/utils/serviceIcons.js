94afc8c80151df97894e431d7aed22515e0122b6
TopDeveloper51 <dragonboy950501@outlook.com>
2023-08-07 17:28:33 +0100

    fix my infos

--- src/utils/serviceIcons.js
+++ src/utils/serviceIcons.js
@@ -2,34 +2,61 @@
 import { BiShoppingBag, BiPencil } from "react-icons/bi";
 import { BsCodeSlash, BsClipboardData } from "react-icons/bs";
 import { AiOutlineMail, AiFillAudio } from "react-icons/ai";
-import { FaChalkboardTeacher, FaProjectDiagram, FaRegFileCode, FaBox, FaCodeBranch, FaCheckDouble, FaServer, FaBug, FaDatabase, FaInternetExplorer, FaChalkboardTeacher, FaCameraRetro, FaPinterest, FaVideo, FaTabletAlt, FaRegNewspaper } from "react-icons/fa";
+import { FaInternetExplorer, FaChalkboardTeacher, FaCameraRetro, FaPinterest, FaVideo, FaTabletAlt, FaRegNewspaper } from "react-icons/fa";
 
 export const serviceIcons = (service) => {
     const serviceID = service.toLowerCase();
     switch (serviceID) {
         case 'web development and design':
-            return <BsCodeSlash />;
+            return <BiShoppingBag />;
         case 'mobile app development':
-            return <FaTabletAlt />;
+            return <BiPencil />;
         case 'database management':
-            return <FaDatabase />;
+            return <AiOutlineMail />;
         case 'api development':
             return <FaInternetExplorer />;
         case 'server configuration and deployment':
-            return <FaServer />;
+            return <FaChalkboardTeacher />;
         case 'version control':
             return <BsCodeSlash />;
         case 'testing and debugging':
-            return <FaBug />;
+            return <FaCameraRetro />;
         case 'continuous integration and deployment':
-            return <FaCodeBranch />;
+            return <FaPinterest />;
         case 'performance optimization':
-            return <FaBox />;
+            return <FaVideo />;
         case 'code development and review':
-            return <FaRegFileCode />;
+            return <BsClipboardData />;
         case 'project planning and management':
-            return <FaProjectDiagram />;
+            return <FaTabletAlt />;
         case 'technical leadership':
-            return <FaChalkboardTeacher />;
+            return <AiFillAudio />;
+        // case 'blog posting':
+        //     return <FaRegNewspaper />;
+
+        // case 'web development and design':
+        //     return <BsCodeSlash />;
+        // case 'mobile app development':
+        //     return <FaTabletAlt />;
+        // case 'database management':
+        //     return <FaDatabase />;
+        // case 'api development':
+        //     return <FaInternetExplorer />;
+        // case 'server configuration and deployment':
+        //     return <FaServer />;
+        // case 'version control':
+        //     return <BsCodeSlash />;
+        // case 'testing and debugging':
+        //     return <FaBug />;
+        // case 'continuous integration and deployment':
+        //     return <FaCodeBranch />;
+        // case 'performance optimization':
+        //     return <FaBox />;
+        // case 'code development and review':
+        //     return <FaRegFileCode />;
+        // case 'project planning and management':
+        //     return <FaProjectDiagram />;
+        // case 'technical leadership':
+        //     return <FaChalkboardTeacher />;
     }
 }

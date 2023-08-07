7efbdcc03dc63d09f635ef539d92beafd4796237
TopDeveloper51 <dragonboy950501@outlook.com>
2023-08-07 17:07:04 +0100

    fix my infos

--- src/utils/serviceIcons.js
+++ src/utils/serviceIcons.js
@@ -2,36 +2,60 @@
 import { BiShoppingBag, BiPencil } from "react-icons/bi";
 import { BsCodeSlash, BsClipboardData } from "react-icons/bs";
 import { AiOutlineMail, AiFillAudio } from "react-icons/ai";
-import { FaInternetExplorer, FaChalkboardTeacher, FaCameraRetro, FaPinterest, FaVideo, FaTabletAlt, FaRegNewspaper } from "react-icons/fa";
+import { FaChalkboardTeacher, FaProjectDiagram, FaRegFileCode, FaBox, FaCodeBranch, FaCheckDouble, FaServer, FaBug, FaDatabase, FaInternetExplorer, FaChalkboardTeacher, FaCameraRetro, FaPinterest, FaVideo, FaTabletAlt, FaRegNewspaper } from "react-icons/fa";
 
 export const serviceIcons = (service) => {
     const serviceID = service.toLowerCase();
     switch (serviceID) {
+        // case 'web development and design':
+        //     return <BiShoppingBag />;
+        // case 'mobile app development':
+        //     return <BiPencil />;
+        // case 'database management':
+        //     return <AiOutlineMail />;
+        // case 'api development':
+        //     return <FaInternetExplorer />;
+        // case 'server configuration and deployment':
+        //     return <FaChalkboardTeacher />;
+        // case 'version control':
+        //     return <BsCodeSlash />;
+        // case 'testing and debugging':
+        //     return <FaBug />;
+        // case 'FaCheckDouble':
+        //     return <FaPinterest />;
+        // case 'performance optimization':
+        //     return <FaVideo />;
+        // case 'code development and review':
+        //     return <BsClipboardData />;
+        // case 'project planning and management':
+        //     return <FaTabletAlt />;
+        // case 'technical leadership':
+        //     return <AiFillAudio />;
+        // case 'blog posting':
+        //     return <FaRegNewspaper />;
         case 'web development and design':
-            return <BiShoppingBag />;
+            return <BsCodeSlash />;
         case 'mobile app development':
-            return <BiPencil />;
+            return <FaTabletAlt />;
         case 'database management':
-            return <AiOutlineMail />;
+            return <FaDatabase />;
         case 'api development':
             return <FaInternetExplorer />;
         case 'server configuration and deployment':
-            return <FaChalkboardTeacher />;
+            return <FaServer />;
         case 'version control':
             return <BsCodeSlash />;
         case 'testing and debugging':
-            return <FaCameraRetro />;
+            return <FaBug />;
         case 'continuous integration and deployment':
-            return <FaPinterest />;
+            return <FaCodeBranch />;
         case 'performance optimization':
-            return <FaVideo />;
+            return <FaBox />;
         case 'code development and review':
-            return <BsClipboardData />;
+            return <FaRegFileCode />;
         case 'project planning and management':
-            return <FaTabletAlt />;
+            return <FaProjectDiagram />;
         case 'technical leadership':
-            return <AiFillAudio />;
-        // case 'blog posting':
-        //     return <FaRegNewspaper />;
+            return <FaChalkboardTeacher />;
     }
 }

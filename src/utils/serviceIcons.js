34714c4cfc754b9fedad9828c95d8b461508d732
TopDeveloper51 <dragonboy950501@outlook.com>
2023-08-08 10:04:50 +0100

    change name

--- src/utils/serviceIcons.js
+++ src/utils/serviceIcons.js
@@ -1,36 +1,36 @@
 
 import { BiShoppingBag, BiPencil } from "react-icons/bi";
 import { BsCodeSlash, BsClipboardData } from "react-icons/bs";
-import { AiOutlineMail, AiFillAudio } from "react-icons/ai";
-import { FaInternetExplorer, FaChalkboardTeacher, FaCameraRetro, FaPinterest, FaVideo, FaTabletAlt, FaRegNewspaper } from "react-icons/fa";
+import { AiOutlineMail, AiFillAudio, AiFillControl, AiFillApi } from "react-icons/ai";
+import { FaProjectDiagram, FaCodeBranch, FaCheckCircle, FaBug, FaJsSquare, FaMobileAlt, FaServer, FaDatabase, FaInternetExplorer, FaChalkboardTeacher, FaCameraRetro, FaPinterest, FaVideo, FaTabletAlt, FaRegNewspaper } from "react-icons/fa";
 
 export const serviceIcons = (service) => {
     const serviceID = service.toLowerCase();
     switch (serviceID) {
         case 'web development and design':
-            return <BiShoppingBag />;
+            return <FaJsSquare />;
         case 'mobile app development':
-            return <BiPencil />;
+            return <FaMobileAlt />;
         case 'database management':
-            return <AiOutlineMail />;
+            return <FaDatabase />;
         case 'api development':
-            return <FaInternetExplorer />;
+            return <AiFillApi />;
         case 'server configuration and deployment':
-            return <FaChalkboardTeacher />;
+            return <FaServer />;
         case 'version control':
-            return <BsCodeSlash />;
+            return <AiFillControl />;
         case 'testing and debugging':
-            return <FaCameraRetro />;
+            return <FaBug />;
         case 'continuous integration and deployment':
-            return <FaPinterest />;
+            return <FaCodeBranch />;
         case 'performance optimization':
-            return <FaVideo />;
+            return <FaCheckCircle />;
         case 'code development and review':
-            return <BsClipboardData />;
+            return <BsCodeSlash />;
         case 'project planning and management':
-            return <FaTabletAlt />;
+            return <FaProjectDiagram />;
         case 'technical leadership':
-            return <AiFillAudio />;
+            return <FaChalkboardTeacher />;
         // case 'blog posting':
         //     return <FaRegNewspaper />;
 
